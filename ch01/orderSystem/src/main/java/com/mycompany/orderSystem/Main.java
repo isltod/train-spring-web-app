efd8a678b25c6cb65aabdcf856d40eb6428372e9
E. K. Lee <wolf@teoal.net>
2025-11-22 23:26:27 +0900

    1장 실습 다시 올리기

---
+++ ch01/orderSystem/src/main/java/com/mycompany/orderSystem/Main.java
@@ -0,0 +1,28 @@
+package com.mycompany.orderSystem;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        CustomerService customerService = context.getBean("customerService", CustomerService.class);
+        List<Customer> customers = customerService.getCustomers();
+        for(Customer customer : customers)
+            System.out.println(customer);
+        Customer newCustomer = new Customer();
+        newCustomer.setId(6);
+        newCustomer.setName("김육");
+        newCustomer.setAddress("제주시");
+        newCustomer.setEmail("kim6@gmail.com");
+        customerService.saveCustomer(newCustomer);
+        Customer customer6 = customerService.getCustomer(6);
+        System.out.println(customer6);
+        customerService.deleteCustomer(1);
+        List<Customer> customerList = customerService.getCustomers();
+        for(Customer customer : customerList)
+            System.out.println(customer);
+    }
+}
