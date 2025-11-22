477250c7c78d35cf27a39c7825650074db249f01
E. K. Lee <wolf@teoal.net>
2025-11-22 23:43:16 +0900

    2장 실습 다시 시작

---
+++ ch02/orderSystem/src/main/java/com/mycompany/ordersystem/CustomerServiceImpl.java
@@ -0,0 +1,35 @@
+package com.mycompany.ordersystem;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service("customerService")
+public class CustomerServiceImpl implements CustomerService {
+    private CustomerRepository customerRepository;
+    public CustomerServiceImpl() {
+
+    }
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Customer getCustomer(long id) {
+        return customerRepository.findById(id);
+    }
+
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    public List<Customer> getCustomersByName(String name) {
+        return customerRepository.findByName(name);
+    }
+
+    public void saveCustomer(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    public void deleteCustomer(long id) {
+        customerRepository.delete(id);
+    }
+}
